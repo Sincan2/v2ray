3fcf4a3d707998ef7e226bb85120bac1837dbf8a
Sincan2 <hadi@the-netwerk.com>
2022-11-07 22:13:01 +0700

    Update updatedll.sh

--- menu/updatedll.sh
+++ menu/updatedll.sh
@@ -13,17 +13,17 @@ rm -r updatedll
 # download
 #
 cd /usr/bin
-wget -O xmenu "https://raw.githubusercontent.com/Hadi-INC/V2Ray/main/menu/xmenu.sh"
-wget -O add-akun "https://raw.githubusercontent.com/Hadi-INC/V2Ray/main/menu/add-akun.sh"
-wget -O updatedll "https://raw.githubusercontent.com/Hadi-INC/V2Ray/main/menu/updatedll.sh"
-wget -O add-akun "https://raw.githubusercontent.com/Hadi-INC/V2Ray/main/menu/add-akun.sh"
-wget -O delete-akun "https://raw.githubusercontent.com/Hadi-INC/V2Ray/main/menu/delete-akun.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/Hadi-INC/V2Ray/main/menu/cert.sh"
-wget -O restart-xray "https://raw.githubusercontent.com/Hadi-INC/V2Ray/main/menu/restart-xray.sh"
-wget -O xmenu "https://raw.githubusercontent.com/Hadi-INC/V2Ray/main/menu/xmenu.sh"
-wget -O auto-pointing "https://raw.githubusercontent.com/Hadi-INC/V2Ray/main/menu/auto-pointing.sh"
-wget -O cek-port "https://raw.githubusercontent.com/Hadi-INC/V2Ray/main/menu/cek-port.sh"
-wget -O xmenu "https://raw.githubusercontent.com/Hadi-INC/V2Ray/main/menu/xmenu.sh"
+wget -O xmenu "https://raw.githubusercontent.com/Sincan2/v2ray/main/menu/xmenu.sh"
+wget -O add-akun "https://raw.githubusercontent.com/Sincan2/v2ray/main/menu/add-akun.sh"
+wget -O updatedll "https://raw.githubusercontent.com/Sincan2/v2ray/main/menu/updatedll.sh"
+wget -O add-akun "https://raw.githubusercontent.com/Sincan2/v2ray/main/menu/add-akun.sh"
+wget -O delete-akun "https://raw.githubusercontent.com/Sincan2/v2ray/main/menu/delete-akun.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/Sincan2/v2ray/main/menu/cert.sh"
+wget -O restart-xray "https://raw.githubusercontent.com/Sincan2/v2ray/main/menu/restart-xray.sh"
+wget -O xmenu "https://raw.githubusercontent.com/Hadi-INC/Sincan2/v2ray/menu/xmenu.sh"
+wget -O auto-pointing "https://raw.githubusercontent.com/Sincan2/v2ray/main/menu/auto-pointing.sh"
+wget -O cek-port "https://raw.githubusercontent.com/Sincan2/v2ray/main/menu/cek-port.sh"
+wget -O xmenu "https://raw.githubusercontent.com/Sincan2/v2ray/main/menu/xmenu.sh"
 
 #
 chmod +x /usr/bin/updatedll
